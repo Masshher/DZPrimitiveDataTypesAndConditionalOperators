80a15a720fd602ef6a59bbee5497825633dfed1e
Maria <cose@list.ru>
2023-04-30 13:11:34 +0300

    add first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 13000;
+        int bonus = 20;
+        int mile = price / bonus;
+
+        System.out.println("Итоговый бонус: " + mile + " миль");
+    }
+}
